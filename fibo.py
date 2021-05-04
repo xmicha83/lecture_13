046cc457b6499c5e87f0d8005d4ebf29036f322c
Eva Michalkova <xmicha83@vutbr.cz>
2021-05-04 17:14:23 +0200

    17:14

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return (recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2))
+
 
 def main():
-    pass
+    prvek = int(input('Zadej prvek:'))
+    posloupnost = [recursive_nth_fibo(n) for n in range(prvek+1)]
+    print(recursive_nth_fibo(prvek))
+    print(posloupnost)
 
 
 if __name__ == '__main__':
